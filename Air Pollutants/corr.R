d54028509ec2c532822df98a5edaa68e2c0cd1ff
someEntropy <diamondgm@gmail.com>
2015-12-17 11:10:47 +0200

    corr done!

--- Air Pollutants/corr.R
+++ Air Pollutants/corr.R
@@ -1,5 +1,19 @@
 corr <- function(directory, threshold = 0){
   
   # Find all files that meet the threshold
+  caseFiles <- list.files(directory, full.names = TRUE)
+
+  cr <- c()
+  
+  for (i in 1:length(caseFiles)) {
+    fileData <- read.csv(caseFiles[i], colClasses = c("NULL", "real", "real"))[complete.cases(read.csv(caseFiles[i])), ]
+    comp <- nrow(fileData)
+    if (comp > threshold) {
+      cr <- c(cr, cor(fileData$sulfate, fileData$nitrate) )
+    } 
+  }
+  return(cr)
+  
   
 }
+
